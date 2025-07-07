388fc247832ec5dbc7f9057acbb99ce0b7fd1640
sujal Prasad <sujalprasad90@gmail.com>
2025-07-07 13:45:47 +0530

    some  more changes

--- training_model.py
+++ training_model.py
@@ -2,5 +2,7 @@ import sklearn
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from haversine import haversine,Unit
 from sklearn.model_selection import train_test_split
+
 
