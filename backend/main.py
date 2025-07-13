dbee6d0b67932f7a1cd6a356a9c347610351ad6c
sujal Prasad <sujalprasad90@gmail.com>
2025-07-14 01:22:11 +0530

    some more debugging

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,15 @@
-from flask import Flask, render_template,request_started
+from flask import Flask, render_template, url_for
+import requests
 
-app=Flask(__name__)
-if(__name__=="__main__"):
+app = Flask(__name__)
+
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")  
+
+if __name__ == "__main__":
     app.run(debug=True)
